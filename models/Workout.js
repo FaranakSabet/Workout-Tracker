4e5951773a90185bf0ca37803d0e310488003208
faranak Sabet <faranaksabet@yahoo.com>
2020-10-25 18:00:17 -0700

    update workout.js

--- models/Workout.js
+++ models/Workout.js
@@ -6,4 +6,36 @@ const workoutSchema = new Schema({
     type: Date,
     required: true,
   },
-  
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      weight: {
+        type: Number,
+        required: true,
+      },
+      reps: {
+        type: Number,
+        required: true,
+      }, 
+      sets: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+})
+
+const Workout = mongoose.model("Workout", workoutSchema) // singular so it will match plural on DB
+
+module.exports = Workout
