149d4ec4ff9612805f3b093de7b43d9399ffa09f
faranak Sabet <faranaksabet@yahoo.com>
2020-10-25 18:46:16 -0700

    third update on server.js

--- server.js
+++ server.js
@@ -13,3 +13,12 @@ const dotenv = require("dotenv")
 const bodyParser = require("body-parser")
 
 dotenv.config()
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static("public"))
+
+const PORT = 3000
